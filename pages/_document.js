aa2fe544ba1da3c5ab615f5250421c33e2689729
Petr Holcak <petrholcak@gmail.com>
2022-12-25 11:14:12 +0100

    Add og:image

--- pages/_document.js
+++ pages/_document.js
@@ -23,8 +23,11 @@ export default function Document () {
       />
       <meta name="keywords" content="Průpravná cvičení, logopedická prevence, cvičení pro předškolní věk, předškoláci, logopedie, dílní funkce, rozvoj zrakového vnímání, rozvoj sluchového vnímání, oromotorika, cvičení na motoriku jazyka, serialita, časová posloupnost, předškolní věk, logopedie v MŠ"></meta>
       <meta name="author" content="Petr Holčák"></meta>
-      <meta property="og:image" content="Prozatím nemáme" />
-      <meta property="og:image:secure_url" content="Prozatím nemáme" /> 
+      <meta property="og:title" content="Logoušek" /> 
+      <meta property="og:description" content="Aplikace pro předškolní děti podporující logopedickou prevenci hravou formou." /> 
+      <meta property="og:site_name" content="logousek" /> 
+      <meta property="og:image" content="https://www.logousek.cz/images/og_image.png" />
+      <meta property="og:image:secure_url" content="https://www.logousek.cz/images/og_image.png" /> 
       <meta property="og:image:type" content="image/jpeg" /> 
       <meta property="og:image:width" content="400" /> 
       <meta property="og:image:height" content="300" />
