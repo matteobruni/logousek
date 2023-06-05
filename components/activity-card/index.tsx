318eef147abba9b17c99927b39627ab790c228f6
Petr Holcak <petrholcak@gmail.com>
2023-06-05 13:18:37 +0200

    LOG-90 - Fix build problem

--- components/activity-card/index.tsx
+++ components/activity-card/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import * as S from './styled'
 
 export type ActivityCardType = {
-  children: React.ReactNode
+  children?: React.ReactNode
   onClick?: () => void
   selected?: boolean
   rotate?: number
